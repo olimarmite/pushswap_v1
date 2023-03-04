d0647dbb301954c2dfb2cb47ca5ab975e7acf3bf
Olivier MARTIN <olimarti@e2r8p4.clusters.42paris.fr>
2023-03-04 05:07:07 +0100

    Fix for 3 args

--- src/sorter/sorter/sort_small.c
+++ src/sorter/sorter/sort_small.c
@@ -6,7 +6,7 @@
 /*   By: olimarti <olimarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/26 19:37:23 by olimarti          #+#    #+#             */
-/*   Updated: 2023/02/26 21:30:11 by olimarti         ###   ########.fr       */
+/*   Updated: 2023/03/04 04:59:04 by olimarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,11 @@ void	sort_small(t_pushswap *pushswap)
 	int	moves;
 
 	i = 0;
-	moves = pushswap->stack_a.item_count / 2;
-	while (i < moves)
-	{
+	moves = 0;
+	if (pushswap->stack_a.item_count > 3)
+		moves = pushswap->stack_a.item_count / 2;
+	while (i++ < moves)
 		pushswap_push(stack_id_b, pushswap);
-		i ++;
-	}
 	if ((moves >= 2)
 		&& pushswap->stack_b.content[0] < pushswap->stack_b.content[1])
 		pushswap_rotate(0, stack_id_b, pushswap);
