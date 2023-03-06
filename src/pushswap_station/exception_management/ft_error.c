a3fb357dbeee689008c28b8fbe30ee9d375339f0
Olivier MARTIN <olimarti@e2r2p3.clusters.42paris.fr>
2023-03-06 18:01:49 +0100

    Add newline after custom error

--- src/pushswap_station/exception_management/ft_error.c
+++ src/pushswap_station/exception_management/ft_error.c
@@ -6,7 +6,7 @@
 /*   By: olimarti <olimarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 09:27:52 by olimarti          #+#    #+#             */
-/*   Updated: 2023/02/27 16:13:39 by olimarti         ###   ########.fr       */
+/*   Updated: 2023/03/06 17:58:46 by olimarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,9 @@
 void	ft_fatal_error(t_pushswap *pushswap, char *error_msg)
 {
 	if (error_msg == NULL || USE_BASIC_ERROR != 0)
-		error_msg = "Error\n";
-	pushswap_free(pushswap);
+		error_msg = "Error";
 	ft_putstr_fd(error_msg, 2);
+	ft_putstr_fd("\n", 2);
+	pushswap_free(pushswap);
 	exit(1);
 }
